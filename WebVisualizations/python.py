747e29f9226a5f425a884c5f0dedd4053f0c66e7
Emily Goldner <eg12700@gmail.com>
2019-10-26 17:37:50 -0500

    Added data page and updated visualization pages

--- WebVisualizations/python.py
+++ WebVisualizations/python.py
@@ -1,1 +1,11 @@
 import pandas as pd 
+
+#import data from csv
+
+data = pd.read_csv("resources/Random_Results.csv")
+
+df = pd.DataFrame(data)
+
+df = df.drop(columns=['Unnamed: 0'])
+
+df.to_html("resources/html_df.html")
